1cb722cd4af8b1b6c75186f706cfe43eb58b8f43
Aires23Matos <aires2matos18@gmail.com>
2024-12-30 16:14:49 +0100

    Creating the Connection Port

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,18 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
+  {
+  parserOptions: {
+    ecmaVersion: 12, // ou 2021
+    sourceType: 'module', // Define o tipo de módulo como ESM
+  },
+  extends: [
+      'eslint:recommended'
+  ],
+  rules: {
+      // Suas regras personalizadas
+  },
+  },
   pluginJs.configs.recommended,
 ];
